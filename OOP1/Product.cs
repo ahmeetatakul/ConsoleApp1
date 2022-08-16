f174347be2f7da18cd393cc910b1e75babbce013
ahmeetatakul <110739612+ahmeetatakul@users.noreply.github.com>
2022-08-16 17:52:33 +0300

    Add files via upload

---
+++ OOP1/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    internal class Product // ürün
+    {
+
+        //unıtprice fiyatı
+        public int Id { get; set; }
+        //kategorisi refans oldğu için kategoriler ikinci sırada Id den hemen sonra yazılır
+        public int CategoryId { get; set; }
+        //ürünİsmi
+        public string ProductName { get; set; }
+        //fiyatı
+        public double UnitPrice { get; set; }
+        //stokAdedi
+        public int UnitınStock { get; set; }
+
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    internal class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " eklendi. ");
+
+        }
+        public void Uptade(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi. ");
+
+        }
+       // public int Topla(int sayı1 , int sayı2)
+       // {
+           // return sayı1 + sayı2;
+
+       // }
+       // public void Topla2(int sayı1, int sayı2)
+       // {
+         //   Console.WriteLine(sayı1+ sayı2);
+            //Topla2 void ilede işlemini yapıyor sıkıntı yok ama bizim onun dönüşüne ihtiyacımız olduğu zaman tekrara kullanamayız
+            //neden returna ihtiyacım var = sonucu başka birşeydede kulllanamak istediğimiz için 
+       // }
+
+        // 2 void git yap git ekle anlamına gelmektedir
+
+        //public void BiseyYap(int sayi)
+        //{
+        // sayi = 99;
+        // 
+
+        
+
+    }
+}
